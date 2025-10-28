3f833d2c05dc8a289efa55ec77bfc4e6bac15231
Justin Bower <jbower2024@my.fit.edu>
2025-10-28 17:33:04 -0400

    Add files via upload

--- tcp_client.c
+++ tcp_client.c
@@ -1,24 +1,22 @@
+// tcp_client.c
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
 #include <stdlib.h>
-#include <sys/types.h> 
+#include <sys/types.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include "utils.h"
-
 
 int main (int argc, char *argv[])
 {
     int socket_desc;
     struct sockaddr_in server_addr;
-    char server_message[100], client_message[100];
-
     struct msg the_message; 
     
     // Command-line input arguments (user provided)
     int externalIndex = atoi(argv[1]); 
-    float initialTemperature = atof(argv[2]); 
+    float externalTemp = atof(argv[2]); 
 
     
     // Create socket:
@@ -44,25 +42,36 @@ int main (int argc, char *argv[])
     }
     printf("Connected with server successfully\n");
     printf("--------------------------------------------------------\n\n");
-       
-    // Package to the sent to server 
-    the_message = prepare_message(externalIndex, initialTemperature); 
+    
+    while (1) {
+        // Package to the sent to server 
+        the_message = prepare_message(externalIndex, externalTemp, false); 
 
-    // Send the message to server:
-    if(send(socket_desc, (const void *)&the_message, sizeof(the_message), 0) < 0){
-        printf("Unable to send message\n");
-        return -1;
-    }
+        // Send the message to server:
+        if(send(socket_desc, (const void *)&the_message, sizeof(the_message), 0) < 0){
+            printf("Unable to send message\n");
+            return -1;
+        }
  
 
-    // Receive the server's response:
-    if(recv(socket_desc, (void *)&the_message, sizeof(the_message), 0) < 0){
-        printf("Error while receiving server's msg\n");
-        return -1;
+        // Receive the server's response:
+        if(recv(socket_desc, (void *)&the_message, sizeof(the_message), 0) < 0){
+            printf("Error while receiving server's msg\n");
+            return -1;
+        }
+        
+        if (the_message.done) {
+            printf("--------------------------------------------------------\n");
+            printf("System stabilized. Final temperature for process %d = %.2f\n", externalIndex, externalTemp);
+            break;
+        }
+        
+        // Update external temperature
+        float centralTemp = the_message.T;
+        externalTemp = (3 * externalTemp + 2 * centralTemp) / 5.0;
+        printf("Updated temperature for process %d = %.2f\n", externalIndex, externalTemp);
+        printf("--------------------------------------------------------\n\n");
     }
-    
-    printf("--------------------------------------------------------\n");
-    printf("Updated temperature sent by the Central process = %f\n", the_message.T);
     
     // Close the socket:
     close(socket_desc);
--- utils.h
+++ utils.h
@@ -5,12 +5,13 @@
 #include <sys/types.h> 
 #include <sys/socket.h>
 #include <arpa/inet.h>
-
+#include <stdbool.h>
 
 struct msg{
     float T;     // Temperature 
     int Index;    // Index indifying the process 
+    bool done;   // Flag to indicate termination
 }; 
 
-struct msg prepare_message(int i_Index, float i_Temperature); 
+struct msg prepare_message(int i_Index, float i_Temperature, bool i_done); 
 
